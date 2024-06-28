d0257a539ff9d81b5f2d256f9e8a4a50d920cf60
Adrian Anchelo <adrian.20191176@iit.ac.lk>
2024-06-28 22:32:06 +0530

    New

---
+++ src/components/misc/Links.js
@@ -0,0 +1,3 @@
+import tw from "twin.macro";
+
+export const PrimaryLink = tw.a`cursor-pointer font-bold text-green-500 border-b-2 border-transparent hocus:border-green-500 hocus:text-green-800 transition duration-300`;
